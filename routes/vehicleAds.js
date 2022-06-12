83e2554f2f46acba21aa2333b3f1b1845f1c18c9
Flemingstar Syiemlieh <flemingsyiemlieh8899@gmail.com>
2022-06-12 18:49:51 +0530

    remove Vehile code

--- routes/vehicleAds.js
+++ routes/vehicleAds.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const router = express.Router()
-const multer = require("multer");
-const upload = require("../utils/multer");
+// const express = require("express");
+// const router = express.Router()
+// const multer = require("multer");
+// const upload = require("../utils/multer");
 
-const { addAds, updateAds, deleteAds } = require("../controller/vehicleAdsController")
-const routes = router.route
-router.route('/add', upload.single("myFile")).post(addAds);
-// router.route.post("/add", upload.single("myFile"), addAds);
-router.route('/update').patch(updateAds);
-router.route('/delete/:id').delete(deleteAds);
+// const { addAds, updateAds, deleteAds } = require("../controller/vehicleAdsController")
+// const routes = router.route
+// router.route('/add', upload.single("myFile")).post(addAds);
+// // router.route.post("/add", upload.single("myFile"), addAds);
+// router.route('/update').patch(updateAds);
+// router.route('/delete/:id').delete(deleteAds);
 
-module.exports = router
+// module.exports = router
